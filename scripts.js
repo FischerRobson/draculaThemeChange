f510821803b9ceefff881240d0cf39a2767b4aea
Robson Fischer <fischerrobson@gmail.com>
2020-06-04 23:48:23 -0300

    update for GitHub

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const draculaMode = {
+    bg: "#282a36",
+    bgPanel: "#44475a",
+    colorHeadings: "#ff79c6",
+    colorText: "#6272a4"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(draculaMode) : changeColors(initialColors)
+})
